ba90e5a49d35510f24eeee6142ec4b5e9c8241c9
Sooyoun Lee <E0046777@u.nus.edu>
2016-08-13 23:43:16 +0800

    Completed ExerciseB6

--- CSExercises/SectionB/ExB6.cs
+++ CSExercises/SectionB/ExB6.cs
@@ -16,13 +16,30 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
-            //YOUR CODE HERE
+            Console.WriteLine("Enter the (x,y) coordinates of two points.");
+            Console.Write("x1:\t");
+            string x1str = Console.ReadLine();
+            Console.Write("y1:\t");
+            string y1str = Console.ReadLine();
+            Console.Write("x2:\t");
+            string x2str = Console.ReadLine();
+            Console.Write("y2:\t");
+            string y2str = Console.ReadLine();
+
+            double x1 = Convert.ToDouble(x1str);
+            double y1 = Convert.ToDouble(y1str);
+            double x2 = Convert.ToDouble(x2str);
+            double y2 = Convert.ToDouble(y2str);
+
+            double distanceBtw = CalculateDistance(x1, y1, x2, y2);
+            Console.WriteLine("The distance between two points is {0}", distanceBtw);
         }
 
         public static double CalculateDistance(double x1, double y1, double x2, double y2)
         {
-            //YOUR CODE HERE
-            return 0;
+            double distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+
+            return distance;
         }
     }
 }
