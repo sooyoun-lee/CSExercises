cbe2c5947638f7c052838ac1f5f267884a0156f6
Sooyoun Lee <E0046777@u.nus.edu>
2016-08-13 21:58:32 +0800

    Completed ExerciseA1

--- CSExercises/SectionA/ExA1.cs
+++ CSExercises/SectionA/ExA1.cs
@@ -19,7 +19,8 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
-            //YOUR CODE HERE
+            Console.WriteLine("Sooyoun Lee");
+            Console.WriteLine("e0046777@u.nus.edu");
         }
     }
 }
