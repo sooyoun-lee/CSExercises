55be28b0865ccc30d26affcf91944ce1127923cc
Sooyoun Lee <E0046777@u.nus.edu>
2016-08-15 16:38:44 +0800

    Completed ExerciseC3

--- CSExercises/SectionC/ExC3.cs
+++ CSExercises/SectionC/ExC3.cs
@@ -22,14 +22,51 @@ namespace CSExercises
             int mark = Convert.ToInt32(Console.ReadLine());
 
             string grade = CalculateGrade(mark);
-            Console.WriteLine("You scored {0} marks which is {1} grade.", grade);
+            Console.WriteLine("You scored {0} marks which is {1} grade.", mark, grade);
 
         }
 
         public static string CalculateGrade(int mark)
         {
-            //YOUR CODE HERE
-            return null;
+            string grade = "";
+            if (mark > 100)
+            {
+                grade = "error";
+            }
+            else
+            {
+                if (mark < 0)
+                {
+                    grade = "error";
+                }
+                else
+                {
+                    if (mark >= 80)
+                    {
+                        grade = "A";
+                    }
+                    else
+                    {
+                        if (mark >= 60)
+                        {
+                            grade = "B";
+                        }
+                        else
+                        {
+                            if (mark >= 40)
+                            {
+                                grade = "C";
+                            }
+                            else
+                            {
+                                grade = "F";
+                            }
+                        }
+                    }
+                }
+            }
+              
+            return grade;
         }
     }
 }
